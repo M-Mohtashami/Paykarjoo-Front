a6bc4b1900bafb659f3aafd9c39baf108023b007
m-mohtashami <m.mohtashami88@gmail.com>
2023-10-14 18:55:05 +0330

    resolve confilict

--- front-end/src/pages/index.js
+++ front-end/src/pages/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Inter } from 'next/font/google';
 import MainLayout from '@/layout/MainLayout';
 
@@ -6,10 +5,6 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return <main></main>;
-=======
-export default function Home() {
-  return <></>;
->>>>>>> 6051fc7cf412f5147e8af520ec12bf0302d1e0dc
 }
 
 Home.getLayout = function getLayout(page) {

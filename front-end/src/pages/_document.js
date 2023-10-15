c3750b5ad463b7457087bf314f39dcef71396d02
m-mohtashami <m.mohtashami88@gmail.com>
2023-10-15 21:13:30 +0330

    add dir rtl to html  and apply yekan font to body

--- front-end/src/pages/_document.js
+++ front-end/src/pages/_document.js
@@ -2,12 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" dir="rtl">
       <Head />
-      <body>
+      <body className="font-yekan">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }

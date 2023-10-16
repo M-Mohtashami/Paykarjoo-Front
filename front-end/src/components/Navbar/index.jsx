b4bfe22372396ec072973dd3e28b831f33aadbaa
Aydin <habibi.idin@gmail.com>
2023-10-16 10:23:56 +0330

    fixed navbar items

--- front-end/src/components/Navbar/index.jsx
+++ front-end/src/components/Navbar/index.jsx
@@ -2,25 +2,25 @@ import Link from "next/link";
 
 export default function Navbar({ onClose }) {
   return (
-    <div className="border-2 border-gray-300 relative p-2 mx-16 mt-5">
-      <button onClick={onClose} className="absolute top-2 right-2 ">
+    <div dir="rtl" className="border-2 border-gray-300 relative p-2  m-5 ">
+      <button onClick={onClose} className="absolute top-2 left-2">
         ×
       </button>
-      <ul className="flex justify-end space-x-4 mr-10">
-        <li>
-          <Link href="/darbareh-man">درباره من</Link>
+      <ul className="flex justify-start gap-10 ">
+        <li className=" ">
+          <Link href="/mabahes">مباحث</Link>
         </li>
-        <li>
-          <Link href="/gallery-tasavir">گالری تصاویر</Link>
-        </li>
-        <li>
-          <Link href="/film">فیلم</Link>
-        </li>
-        <li>
+        <li className=" ">
           <Link href="/podcast">پادکست</Link>
         </li>
-        <li>
-          <Link href="/mabahes">مباحث</Link>
+        <li className=" ">
+          <Link href="/film">فیلم</Link>
+        </li>
+        <li className=" ">
+          <Link href="/gallery-tasavir">گالری تصاویر</Link>
+        </li>
+        <li className=" ">
+          <Link href="/darbareh-man">درباره من</Link>
         </li>
       </ul>
     </div>
